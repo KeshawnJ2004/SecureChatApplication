561b5dd10df823e51b0b59ac3ec2d8f18d6b83b6
KeshawnJ2004 <keshawn_j@icloud.com>
2025-04-15 13:38:21 -0400

    Initital commit: New Spring Boot project generated via Spring Initializr. 1 warning -> spring.application.name=SecureChat: It'll probably be used later, therefore warning will be ignored

---
+++ src/test/java/com/KeshawnJ2004/securechat/SecureChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KeshawnJ2004.securechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
